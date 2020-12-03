2d969f173c1af21b873b7e7d3162ac1916766a34
Joey Wildman <josephwildman88@gmail.com>
2020-12-03 14:10:14 -0500

    part1 revised

--- Part 1.py
+++ Part 1.py
@@ -10,11 +10,18 @@
 if __name__ == '__main__':
     file1 = open("Part 1.txt", "r")
     numDict = {}
+    #part 1
     for line in file1.readlines():
         numDict[int(line)] = int(line)
     for key in numDict:
         if (2020-key) in numDict:
             print(key, 2020-key)
+    #part 2
+    for key in numDict:
+        sum = (2020-key)
+        for key2 in numDict:
+            if (sum-key2) in numDict:
+                print(key, key2, sum-key2)
 
 
 
